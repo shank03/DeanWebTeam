f3d1c77beee7b0eddb724c6abf5d284640c0e55b
Shashank Verma <shashank.verma2002@gmail.com>
2023-02-01 19:57:01 +0530

    DB: Update structure
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- other/ref_db.sql
+++ other/ref_db.sql
@@ -11,7 +11,7 @@ CREATE TABLE professor (
 );
 
 CREATE TABLE student (
-    registration_number INT PRIMARY KEY,
+    registration_number VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -22,29 +22,19 @@ CREATE TABLE student (
 );
 
 CREATE TABLE course (
-    course_code VARCHAR(255) PRIMARY KEY,
+    course_code VARCHAR(255) NOT NULL,
     course_name VARCHAR(255) NOT NULL,
     semester INT NOT NULL,
     credits INT NOT NULL,
     branch VARCHAR(255) NOT NULL,
     stream VARCHAR(255) NOT NULL,
     course_type VARCHAR(255) NOT NULL,
-    PRIMARY KEY (course_name,semester)
+    PRIMARY KEY (course_code, semester)
 );
 
-CREATE TABLE marks_dist_theory (
-    course_code VARCHAR(255),
+CREATE TABLE marks_distribution (
+    course_code VARCHAR(255) NOT NULL,
     mid_semester_exam INT NOT NULL,
-    end_semester_exam INT NOT NULL,
-    teacher_assessment INT NOT NULL,
-    semester INT NOT NULL,
-    dist_year INT NOT NULL,
-    PRIMARY KEY (course_code,semester,dist_year),
-    FOREIGN KEY (course_code) REFERENCES course(course_code)
-);
-
-CREATE TABLE marks_dist_practical (
-    course_code VARCHAR(255),
     end_semester_exam INT NOT NULL,
     teacher_assessment INT NOT NULL,
     semester INT NOT NULL,
@@ -55,7 +45,7 @@ CREATE TABLE marks_dist_practical (
 
 CREATE TABLE marks (
     course_code VARCHAR(255) NOT NULL,
-    student_registration_number INT NOT NULL,
+    student_registration_number VARCHAR(255) NOT NULL,
     mid_semester_exam INT NOT NULL DEFAULT 0,
     end_semester_exam INT NOT NULL DEFAULT 0,
     teacher_assessment INT NOT NULL DEFAULT 0,
@@ -67,12 +57,13 @@ CREATE TABLE marks (
 );
 
 CREATE TABLE professor_allotment (
-    employee_id INT REFERENCES professor(employee_id),
+    employee_id INT NOT NULL,
     course_code VARCHAR(255) REFERENCES course(course_code),
     branch VARCHAR(255) NOT NULL,
     semester INT NOT NULL,
     d_year INT NOT NULL,
-    PRIMARY KEY (employee_id,course_code,semester,d_year)
+    PRIMARY KEY (employee_id,course_code,semester,d_year),
+    FOREIGN KEY (employee_id) REFERENCES professor(employee_id)
 );
 
 CREATE TABLE admin (
@@ -80,34 +71,70 @@ CREATE TABLE admin (
     password_hash VARCHAR(255) NOT NULL,
     course_entry boolean NOT NULL DEFAULT FALSE,
     grade_entry boolean NOT NULL DEFAULT FALSE,
-    semester INT NOT NULL
+    semester INT NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES professor(employee_id)
 );
 
 # Queries
 
+# password -> '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu' -> '1234567'
+
 # Change entry status
 UPDATE admin SET course_entry = true, grade_entry = true;
 
-INSERT INTO professor (employee_id, first_name, last_name, phone_number, email, password_hash, date_of_join)
-VALUES (1116, 'Dr. Dushyant', 'Kumar Singh', '1234567890', 'dushyant@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01');
-
-INSERT INTO course (course_code, name, semester, credits, branch, stream, course_type)
+# 3rd semester
+INSERT INTO course (course_code, course_name, semester, credits, branch, stream, course_type)
 VALUES ('CS13101', 'Data Structures', 3, 4, 'Computer Science', 'B.Tech', 'theory'),
-('CS15101', 'Microprocessor and its Application', 5, 3, 'Computer Science', 'B.Tech'),
-('CS15102', 'Operating Systems', 5, 4, 'Computer Science', 'B.Tech'),
-('CS15103', 'Database Management System', 5, 4, 'Computer Science', 'B.Tech'),
-('CS15104', 'Object Oriented Modeling', 5, 4, 'Computer Science', 'B.Tech'),
-('CS15105', 'Operation Research', 5, 3, 'Computer Science', 'B.Tech'),
-('CS15106', 'Computer Architecture', 5, 3, 'Computer Science', 'B.Tech'),
-('CS15201', 'Systems Calls Lab', 5, 2, 'Computer Science', 'B.Tech'),
-('CS15202', 'Microprocessor', 5, 2, 'Computer Science', 'B.Tech'),
-('CS15203', 'Operating Systems', 5, 2, 'Computer Science', 'B.Tech'),
-('CS15204', 'Database System', 5, 2, 'Computer Science', 'B.Tech');
+('CS13201', 'Object Oriented Programming', 3, 2, 'Computer Science', 'B.Tech', 'practical');
 
-INSERT INTO course (course_code, name, semester, credits, branch, stream, course_type)
+# 5th semester
+INSERT INTO course (course_code, course_name, semester, credits, branch, stream, course_type)
+VALUES ('CS15101', 'Microprocessor and its Application', 5, 3, 'Computer Science', 'B.Tech', 'theory'),
+('CS15102', 'Operating Systems', 5, 4, 'Computer Science', 'B.Tech', 'theory'),
+('CS15103', 'Database Management System', 5, 4, 'Computer Science', 'B.Tech', 'theory'),
+('CS15104', 'Object Oriented Modeling', 5, 4, 'Computer Science', 'B.Tech', 'theory'),
+('CS15105', 'Operation Research', 5, 3, 'Computer Science', 'B.Tech', 'theory'),
+('CS15106', 'Computer Architecture', 5, 3, 'Computer Science', 'B.Tech', 'theory'),
+('CS15201', 'Systems Calls Lab', 5, 2, 'Computer Science', 'B.Tech', 'practical'),
+('CS15202', 'Microprocessor', 5, 2, 'Computer Science', 'B.Tech', 'practical'),
+('CS15203', 'Operating Systems', 5, 2, 'Computer Science', 'B.Tech', 'practical'),
+('CS15204', 'Database System', 5, 2, 'Computer Science', 'B.Tech', 'practical');
+
+# 6th semester
+INSERT INTO course (course_code, course_name, semester, credits, branch, stream, course_type)
 VALUES ('CS16101', 'Embedded Systems', 6, 3, 'Computer Science', 'B.Tech', 'theory'),
 ('CS16102', 'Compiler Construction', 6, 3, 'Computer Science', 'B.Tech', 'theory'),
 ('CS16103', 'Data Mining', 6, 3, 'Computer Science', 'B.Tech', 'theory'),
 ('CS16104', 'Cryptography & Network Security', 6, 3, 'Computer Science', 'B.Tech', 'theory'),
 ('CS16105', 'Computer Networks', 6, 4, 'Computer Science', 'B.Tech', 'theory'),
 ('CS16106', 'Software Engineering', 6, 3, 'Computer Science', 'B.Tech', 'theory');
+
+# M.Tech
+INSERT INTO course (course_code, course_name, semester, credits, branch, stream, course_type)
+VALUES ('CS21101', 'Data Science: Concepts and Methodologies', 5, 4, 'Computer Science and Engineering', 'M.Tech', 'theory'),
+('CS21201', 'Programming Lab - 1', 5, 4, 'Computer Science and Engineering', 'M.Tech', 'practical'),
+('CS21202', 'Programming Lab - 2', 5, 4, 'Computer Science and Engineering', 'M.Tech', 'practical'),
+('CS22201', 'Machine Learning', 5, 4, 'Computer Science and Engineering', 'M.Tech', 'theory');
+
+INSERT INTO student (registration_number, first_name, last_name, email, password_hash, stream, branch, semester)
+VALUES ('20204022', 'Amit', 'Kumar', 'amit.20204022@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('20204085', 'Hitesh', 'Mitruka', 'hitesh.20204085@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('20204120', 'Naman', 'Aggrawal', 'naman.20204120@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('20204156', 'Priyav', 'Kaneria', 'priyav.20204156@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('20204176', 'Sanskar', 'Omar', 'sanskar.20204176@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('20204184', 'Shashank', 'Verma', 'shashank.20204184@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'B.Tech', 'Computer Science', 5),
+('2018PR01', 'Saurabh', 'last_name', 'saurabh@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'M.Tech', 'Computer Science and Engineering', 5),
+('2018MT11', 'Ankur', 'last_name', 'saurabh@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 'M.Tech', 'Computer Science and Engineering', 5);
+
+INSERT INTO professor (employee_id, first_name, last_name, phone_number, email, password_hash, date_of_join)
+VALUES (1116, 'Dr. Dushyant', 'Kumar Singh', '1234567890', 'dushyant@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1111, 'Dr. Ranvijay', 'Singh', '1234567890', 'ranvijay@mnnii.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1112, 'Lt. (Dr.) Divya', 'Kumar', '1234567890', 'divya@mnnii.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1113, 'Dr. Mayank', 'Pandey', '1234567890', 'mayank@mnnii.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1114, 'Prof. Neeraj', 'Tyagi', '1234567890', 'neeraj@mnnii.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1115, 'Dr. Dinesh', 'Singh', '1234567890', 'dinesh@mnnii.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1117, 'Dr. Shashwati', 'Banerjea', '1234567890', 'shashwati@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01'),
+(1118, 'Dr. Srasij', 'Tripathi', '1234567890', 'sarsij@mnnit.ac.in', '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', '2010-01-01');
+
+INSERT INTO admin (employee_id, password_hash, course_entry, grade_entry, semester)
+VALUES (1116, '$2y$12$NQjkpTMrmkCcOynIgiHiG.VRmzIMCEMxKsFLuvEkRYYequD2V4GNu', 0, 0, 5);
